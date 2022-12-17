a2611973e4137e63bcc394faba7c4e2ca33db9bd
MahmoudDawood <mm.dawood16@yahoo.com>
2022-12-17 23:54:15 +0200

    Day 234 - 6 kyu

---
+++ Shortest steps to a number 6.js
@@ -0,0 +1,11 @@
+function shortestStepsToNum(num) {
+  let steps = 0
+  while(num > 1)
+    num % 2 ? num-- : num /= 2, steps++
+  return steps
+}
+
+console.log(shortestStepsToNum(1), 0);
+console.log(shortestStepsToNum(12), 4);
+console.log(shortestStepsToNum(16), 4);
+console.log(shortestStepsToNum(71), 9);
